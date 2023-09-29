6da071db365d4cac39c70fc57cb350dddec20d27
JRussellUX <146155727+JRussellUX@users.noreply.github.com>
2023-09-29 13:16:56 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portal Page</title>
+    <link href="css/styles.css" typre="text/css"
+    rel="stylesheet">
+</head>
+<body>
+    <h1><a href="index.php">Jennifer's Portal</a></h1>
+    <h2>The navigation below represents our BIG assignments</h2>
+    <div id="wrapper">
+        <nav>
+        <ul>
+        <li><a href="">Switch</a></li>
+        <li><a href="">Troubleshoot</a></li>
+        <li><a href="">Calculator</a></li>
+        <li><a href="">Email</a></li>
+        <li><a href="">Database</a></li>
+        <li><a href="">Gallery</a></li>
+        </ul>
+        </nav>
+
+    <main>
+        <h2>About Jennifer</h2>
+        <img class="right" src="images/jennifer.jpeg" alt="Jennifer">
+        <p>I am a mother to a daughter who just entered middle school.
+            I also have two bunnies that are a handful, and a dog who is
+            the best dog ever! I worked in healthcare for eleven years
+            and realized during the pandemic I needed to switch careers for my daughters health.
+            After deciding web design was the path I wanted to take, I joined an
+            online bootcamp and enjoyed every second. I've worked as a freelancer ever since.
+            Now I am excited to work towards my degree in a feild I enjoy so much!
+            Outside of school and work, I enjoy playing video games, hiking, and baking with my daughter.
+        </p>
+        
+
+    </main>
+
+    <aside>
+        <h2>Weekly Class Exercises</h2>
+        <h3>Week 2</h3>
+        <ol>
+          <li><a href="var.php">var.php</a>
+          <li><a href="">var2.php</a>
+          <li><a href="">something</a>
+          <li><a href="">something else</a>
+        </ol>
+
+    </aside>
+</body>
+</html>
